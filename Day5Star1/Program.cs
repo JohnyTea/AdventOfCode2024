d323d17799afe11994b28e2bccf0806ef6db15c0
lcydejko <luk.cydejko@gmail.com>
2024-12-05 16:47:02 +0100

    submite day 5

---
+++ Day5Star1/Program.cs
@@ -0,0 +1,68 @@
+﻿string[] inputData = File.ReadAllLines("Data.txt");
+List<string> firstSection = new List<string>();
+List<string> secondSection = new List<string>();
+
+int result = 0;
+
+foreach (string line in inputData)
+{
+	if(line == string.Empty)
+	{
+		break;
+	}
+	firstSection.Add(line);
+}
+
+for (int i = firstSection.Count + 1; i < inputData.Length; i++)
+{
+	secondSection.Add(inputData[i]);
+}
+Dictionary<int, List<int>> rules = new Dictionary<int, List<int>>();
+
+foreach (string line in firstSection)
+{
+	string[] lineSplit = line.Split("|"); 
+	int key = int.Parse(lineSplit[0]);
+	int newValue = int.Parse(lineSplit[1]);
+
+	if (rules.TryGetValue(key, out var value))
+	{
+		value.Add(newValue);
+	}
+	else
+	{
+		rules.Add(key, [newValue]);
+	}
+}
+
+foreach (string line in secondSection)
+{
+	string[] update = line.Split(",");
+	int[] updateNumbers = update.Select(x => int.Parse(x)).ToArray();
+	bool isCorrectUpdate = true;
+	for (int i = 0; i < updateNumbers.Length; i++)
+	{
+		int currentNumber = updateNumbers[i];
+		List<int> currentNumberRules = rules[currentNumber];
+
+		foreach (int number in updateNumbers.Take(i))
+		{
+			if (currentNumberRules.Contains(number))
+			{
+				isCorrectUpdate = false;
+			}
+		}
+	}
+
+	if (updateNumbers.Length % 2 == 0)
+	{
+		Console.WriteLine("no middle number");
+	}
+	if (isCorrectUpdate)
+	{
+		result += updateNumbers[updateNumbers.Length / 2];
+
+	}
+}
+
+Console.WriteLine(result);
---
+++ Day5Star2/Program.cs
@@ -0,0 +1,94 @@
+﻿using System.Linq;
+
+string[] inputData = File.ReadAllLines("Data.txt");
+List<string> firstSection = new List<string>();
+List<string> secondSection = new List<string>();
+
+int result = 0;
+
+foreach (string line in inputData)
+{
+	if (line == string.Empty)
+	{
+		break;
+	}
+	firstSection.Add(line);
+}
+
+for (int i = firstSection.Count + 1; i < inputData.Length; i++)
+{
+	secondSection.Add(inputData[i]);
+}
+Dictionary<int, List<int>> rules = new Dictionary<int, List<int>>();
+
+foreach (string line in firstSection)
+{
+	string[] lineSplit = line.Split("|");
+	int key = int.Parse(lineSplit[0]);
+	int newValue = int.Parse(lineSplit[1]);
+
+	if (rules.TryGetValue(key, out var value))
+	{
+		value.Add(newValue);
+	}
+	else
+	{
+		rules.Add(key, [newValue]);
+	}
+}
+
+foreach (string line in secondSection)
+{
+	string[] update = line.Split(",");
+	int[] updateNumbers = update.Select(x => int.Parse(x)).ToArray();
+	bool isCorrectUpdate = true;
+	for (int i = 0; i < updateNumbers.Length; i++)
+	{
+		int currentNumber = updateNumbers[i];
+		List<int> currentNumberRules = rules[currentNumber];
+
+		foreach (int number in updateNumbers.Take(i))
+		{
+			if (currentNumberRules.Contains(number))
+			{
+				isCorrectUpdate = false;
+			}
+		}
+	}
+
+	if (updateNumbers.Length % 2 == 0)
+	{
+		Console.WriteLine("no middle number");
+	}
+	if (!isCorrectUpdate)
+	{
+		List<int> correctedUpdate = FixOrder(updateNumbers);
+		result += correctedUpdate[updateNumbers.Length / 2];
+	}
+}
+
+Console.WriteLine(result);
+
+List<int> FixOrder(int[] incorrectOrderNumbers)
+{
+	List<int> newOrder = new List<int>();
+	foreach (int incorrectOrderNumber in incorrectOrderNumbers)
+	{
+		for (int i = 0; i < newOrder.Count; i++)
+		{
+			if (rules[incorrectOrderNumber].Contains(newOrder[i]))
+			{
+				newOrder.Insert(i,incorrectOrderNumber);
+				break;
+			}
+		}
+
+
+		if (!newOrder.Contains(incorrectOrderNumber))
+		{
+			newOrder.Add(incorrectOrderNumber);
+		}
+	}
+
+	return newOrder;
+}
